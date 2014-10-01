b6d5273dfd08d1e3c0d54425ef0db6d7e9cc35e1
servedbysam <Sam Peters>
2014-10-01 18:41:55 -0400

    see previous
    i just don’t know how to save things apparently

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+/*
+
+----- PSEUDOCODE --------
+
+Get userChoice for question1.
+Limit possible answers based on day(M, T, W, Th, F, Su).
+Get userChoice for question2.
+If userChoice = sitcom, narrow possible outcomes to exclude dramas
+Else if userChoice = drama, exclude sitcoms
+Get userChoice for question3.
+If userChoice = early, limit possible outcomes to shows airing before 9:30 p.m.
+Else if userChoice = late, limit possible outcomes to shows airing after 9:00 p.m.
+Get userChoice for question4.
+If userChoice = no, don't allow results from cable networks.
+
+Display result.
+
+*/
+
+/* ALSO
+
+IDEA: I think i want to change the background to a photo from the resulting show. Am I getting to ambitious? Probably.
+
+*/
